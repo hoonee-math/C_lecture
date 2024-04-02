c530339776aa727fbd32c4504abf138a011c4900
hoonee-math <kwanghoon041@gmail.com>
2024-04-02 15:47:43 +0900

    관계 연산 예제 실습

--- C_lecture/ctest.c
+++ C_lecture/ctest.c
@@ -2,49 +2,27 @@
 
 int main(void)
 {   
-    int x,y,add,sub,mul,div,mod;
-    printf("정수1을 입력학세요 : ");
-    scanf("%d", &x);
-/*scanf 함수
+    int x, y;
+    char a, b;
+    printf("문자 2개를 입력하세요: ");
+    scanf("%c %c",&a, &b);
+    
+    printf("입력한 첫번째 문자 %c의 아스키 10진수는 %d 입니다.\n",a,a);
+    printf("입력한 두번째 문자 %c의 다음 문자는 %c 입니다\n", a, a+1);
+    printf("(%c < %c)는 %d 입니다\n",a,a+1, (a<a+1));
 
-& (address operator, 주소 연산자, 앤퍼센트)
-a라고 하는 변수의 방의 주소를 찾아가서 지금 입력된 값을 이 a방에 넣어줘.
+    printf("정수 두개를 입력하세요 : ");
+    scanf("%d %d",&x,&y);
+    printf("(%d >= %d)는 %d 입니다\n",x,y,(x>=y));
+    printf("(%d == %d)는 %d 입니다\n",x,y,(x==y));
+    printf("(%d != %d)는 %d 입니다\n",x,y,(x!=y));
 
-★★★★
 
-int형 일 때는 %d 로 받아주기
-float형 일 때는 %f 로 받아주기
-double형 일 때는 %lf 로 받아주기*/
-    printf("정수2를 입력하세요 : ");
-    scanf("%d", &y);
-
-    add = x+y;
-    sub = x-y;
-    mul = x*y;
-    div = x/y;
-    mod = x%y;
-
-    printf("%d, %d, %d, %d, %d", add,sub,mul,div,mod);
     return 0;
+   /*char a;
+   printf("문자 두개를 입력해 주세요 : ");
+   scanf("%c",&a);
+   printf("%c",a);
+   return 0;*/
 }
 
-
-/* swap 함수 포인터 변수를 이용하여 변수값 바꾸기
-void swap(int *a, int *b);
-
-int main()
-{
-    int a = 3, b=5;
-    swap(&a, &b);
-    printf("%d, %d",a,b);
-    return 0;
-}
-
-void swap(int *a, int *b)
-{
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-
-}*/
