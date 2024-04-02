2db7332eda55ae8baa1ec53c696d3847d21e1237
hoonee-math <kwanghoon041@gmail.com>
2024-04-02 14:47:40 +0900

    연산자 연습

--- C_lecture/ctest.c
+++ C_lecture/ctest.c
@@ -1,5 +1,35 @@
 #include<stdio.h>
 
+int main(void)
+{   
+    int x,y,add,sub,mul,div,mod;
+    printf("정수1을 입력학세요 : ");
+    scanf("%d", &x);
+/*scanf 함수
+
+& (address operator, 주소 연산자, 앤퍼센트)
+a라고 하는 변수의 방의 주소를 찾아가서 지금 입력된 값을 이 a방에 넣어줘.
+
+★★★★
+
+int형 일 때는 %d 로 받아주기
+float형 일 때는 %f 로 받아주기
+double형 일 때는 %lf 로 받아주기*/
+    printf("정수2를 입력하세요 : ");
+    scanf("%d", &y);
+
+    add = x+y;
+    sub = x-y;
+    mul = x*y;
+    div = x/y;
+    mod = x%y;
+
+    printf("%d, %d, %d, %d, %d", add,sub,mul,div,mod);
+    return 0;
+}
+
+
+/* swap 함수 포인터 변수를 이용하여 변수값 바꾸기
 void swap(int *a, int *b);
 
 int main()
@@ -17,4 +47,4 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 
-}
+}*/
